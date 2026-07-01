01fb1a7564395bbc4c346d43363679c1b2d029ce
yun-jun-hyengh <77119415+yun-jun-hyengh@users.noreply.github.com>
2026-07-02 00:41:51 +0900

    Update database.py

--- config/database.py
+++ config/database.py
@@ -1,8 +1,8 @@
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncConnection
 
-#DATABASE_URL = "mysql+aiomysql://root:123456@localhost:3306/board";
-DATABASE_URL = "mysql+aiomysql://root:123456@localhost:5000/board";
+DATABASE_URL = "mysql+aiomysql://root:123456@localhost:3306/board";
+#DATABASE_URL = "mysql+aiomysql://root:123456@localhost:5000/board";
 
 # Engine 설정
 engine = create_async_engine(DATABASE_URL, echo=True, pool_pre_ping=True);
